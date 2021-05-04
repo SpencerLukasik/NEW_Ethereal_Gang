a024e520ab0e59da350c488489eec4fb8973c765
Kyara Valdez <kya.eliza@gmail.com>
2021-05-03 19:50:47 -0500

    CUSTOMIZATION SCREEN! A new difficulty level :)

--- Assets/Resources/SoundManagerScript.cs
+++ Assets/Resources/SoundManagerScript.cs
@@ -1,9 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.Audio;
+using System;
 using UnityEngine;
 
 public class SoundManagerScript : MonoBehaviour
 {
+    // public Music[] music;
+
+    // void Awake(){
+    //     foreach(Music m in music)
+    //     {
+    //         m.source = gameObject.AddComponent<AudioSource>(); 
+    //         m.source.clip = m.clip;
+
+    //         // m.source.volume = m.volume;
+    //         // m.source.pitch = m.pitch;
+            
+    //     }
+    // }
+
+    // public void Play(string name)
+    // {
+    //     Music m = Array.Find(music, song => song.name == name);
+    //     m.source.Play();
+    // }
+
+    // void Start(){
+    //     Play("My Innermost Apocalypse");
+    // }
+    //}
     // Start is called before the first frame update
     public static AudioClip ambiance;
     public static AudioClip titleScreen;
@@ -11,7 +35,7 @@ public class SoundManagerScript : MonoBehaviour
     void Start()
     {
         titleScreen = Resources.Load<AudioClip> ("TitleScreenMusic");
-        ambiance = Resources.Load<AudioClip> ("Ambiance");
+        ambiance = Resources.Load<AudioClip> ("My Innermost Apocalypse");
         audioSrc = GetComponent <AudioSource> ();
         PlayAmbiance();
         //PlayTitleMusic();
